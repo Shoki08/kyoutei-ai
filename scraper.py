c07b9a650afad4fc10871346262c2a95e580bef7
Shoki08 <yamakou1623@gmail.com>
2025-11-20 12:53:37 +0900

    scraper.py を更新

--- scraper.py
+++ scraper.py
@@ -16,7 +16,6 @@ DATA_DIR = "docs/data"
 os.makedirs(DATA_DIR, exist_ok=True)
 
 USER_AGENTS = [
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
 ]
 
